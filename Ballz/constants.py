ceafeaa5cf25afd192a4cba8544e520c88e5707c
Hampuse1 <hampuse1@hotmail.com>
2021-10-24 09:30:59 +0200

    Updates

--- Ballz/constants.py
+++ Ballz/constants.py
@@ -12,3 +12,19 @@ class Ball(Entity):
         self.scale = 0.1
         self.x = 0
         self.y = -4
+
+class Brick(Entity):
+
+    def __init__(self, x, hits = 5):
+        super().__init__()
+        self.model = "cube"
+        self.color = color.green
+        self.scale = 0.45
+        self.x = x
+        self.y = 1.5
+        self.hits = hits
+        self.text = str(hits)
+
+    def move_down(self):
+        self.y -= 0.45
+
--- Ballz/game.py
+++ Ballz/game.py
@@ -1,4 +1,34 @@
-from ursina import *
+from constants import *
+
+"""
+def addbricks(level):
+    for i in range(-5,6,1):
+        Brick(window.aspect_ratio*i, level)
+
+
+
+addbricks(5)
+app.run()"""
+
 
 app = Ursina(size=(WIDTH, HEIGHT), position= ((FULLWIDTH-WIDTH)/2,(FULLHEIGHT-HEIGHT)/2))
+level = 1
+camera.orthographic = True
+camera.fov = 4
+Text.default_resolution *= 2
+bricks = []
+
+for x in [-0.25 + x * 0.5 for x in range(-2, 4)]:
+    print(x)
+    bricks.append(Brick(x, level))
+
+def update():
+    for b in bricks:
+        b.move_down()
+    
+    time.sleep(1)
+        
+    
 app.run()
+
+

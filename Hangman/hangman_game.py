9ad924ca14e83472904553e38ef89ca9326c43fa
Hampuse1 <hampuse1@hotmail.com>
2021-12-06 14:47:45 +0100

    Random english words
    TODO: Some letters not showing up

--- Hangman/hangman_game.py
+++ Hangman/hangman_game.py
@@ -1,73 +1,69 @@
 from UClasses import *
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print(len(letters))
 
 app = Ursina()
 
 window.fps_counter.enabled = False
 window.exit_button.enabled = False
 
-class ClickerButton(Button):
 
-    def __init__(self, game, letter, x, y):
+class ClickerButton(Button):
+    def __init__(self, game_object, letter, pos_x, pos_y):
         super().__init__()
-        self.text = Text(text=letter, scale=2, x=x-0.025, y=y+0.025)
-        self.game = game
+        self.text = Text(text=letter, scale=2, x=pos_x - 0.025, y=pos_y + 0.025)
+        self.game = game_object
         self.letter = letter
-        self.x = x
-        self.y = y
+        self.x = pos_x
+        self.y = pos_y
         self.scale = 0.1
         self.disabled = False
-        self.highlight_color = self.color.tint(.2)
-        self.pressed_color = self.color.tint(-.2)
+        self.highlight_color = self.color.tint(0.2)
+        self.pressed_color = self.color.tint(-0.2)
         self.color = color.azure
 
     def on_click(self):
+        self.disabled = True
+
         if self.letter in self.game.word:
             self.color = color.green
-            self.disabled = True
-            self.game.updateword(self.letter)
+            self.game.update_word(self.letter)
         else:
             self.color = color.red
-            self.disabled = True
-            self.game.wrongcount += 1
+            self.game.wrong_count += 1
 
 
-class Game():
+class Game:
     def __init__(self):
-        with open('words.txt') as f:
-            lines = f.readlines()
-        self.word = random.choice(lines)
+        with open("words.txt") as f:
+            words = f.readlines()
+        self.word = random.choice(words).upper().strip("\n")
         self.spaces = ["_" for _ in self.word]
         self.wrong_words = []
         self.right_words = []
-        self.wrongcount = 0
+        self.wrong_count = 0
 
-    def updateword(self, letter):
+    def update_word(self, letter):
         for i in range(len(self.word)):
-            print(i,len(self.word), len(self.spaces))
             if self.word[i] == letter:
                 self.spaces[i] = letter
 
 
 def update():
-    if game.wrongcount >= 10:
+    if game.wrong_count >= 10:
         print("Game over")
-    t = ""
-    for c in game.spaces:
-        t += c + " "
+    t = "".join(c + " " for c in game.spaces)
     lines.text = t
 
 
 game = Game()
 buttons = []
-l = 0
-lines = Text(text="", scale = 8)
-lines.origin = ((lines.get_width()+lines.get_x())/2, 0.25)
-for y in range(-2,-4,-1):
-    for x in range(-6,7):
-        buttons.append(ClickerButton(game, letters[l], x/8, y/8))
-        l += 1
+letter_number = 0
+lines = Text(text="", scale=8)
+lines.origin = ((lines.get_width()) / 2, 0.25)
+for y in range(-2, -4, -1):
+    for x in range(-6, 7):
+        buttons.append(ClickerButton(game, letters[letter_number], x / 8, y / 8))
+        letter_number += 1
 
 app.run()
